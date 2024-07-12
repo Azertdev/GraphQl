5866624fd7aece88eb91bc5151811823a35dca9e
cnzale <azermax123zale@gmail.com>
2024-07-12 19:51:02 +0000

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
 import path from "path";
+import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), import("tailwindcss"), import("autoprefixer")],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
     },
   },
 });
